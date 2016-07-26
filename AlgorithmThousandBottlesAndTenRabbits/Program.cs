40cdb77a349e3c722da084508c3eeb96540ef0fd
Tatyana Durova <durova@octonus.com>
2016-07-26 17:49:10 +0300

    Add reading for bottle's number

--- AlgorithmThousandBottlesAndTenRabbits/Program.cs
+++ AlgorithmThousandBottlesAndTenRabbits/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AlgorithmThousandBottlesAndTenRabbits
@@ -10,6 +11,26 @@ namespace AlgorithmThousandBottlesAndTenRabbits
     {
         static void Main(string[] args)
         {
+            ushort n = 0;
+
+            Console.WriteLine("Enter the number of the poisoned bottle: ");
+            while (n == 0)
+            {
+                string s = Console.ReadLine();
+                if (ushort.TryParse(s, out n) && n>0 )
+                {
+                    Console.WriteLine("Poisoned bottle's number = "+ n);
+                    Console.WriteLine("Remember, by default, algorithm work for botte's number from 1 to 1000.");
+                }
+                else
+                {
+                    Console.WriteLine("Please, try again! Your number was invalid.");
+                }
+            }
+            Console.WriteLine("Start algorithm processing...");
+            Console.ReadLine();
+
+
         }
     }
 }
